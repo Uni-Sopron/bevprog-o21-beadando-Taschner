06adc74c3f9941a45f6d4895569c22f4696a6094
Mate Hegyhati <hegyhati@users.noreply.github.com>
2022-01-27 22:16:25 +0100

    Update vizsga.js

--- vizsga.js
+++ vizsga.js
@@ -17,4 +17,11 @@ function bubbles () {
         i++
     }
 }
+
+
+  
+let bubble1 = prompt ('Hany buborekot szeretnel?')
+let bubble2 = prompt ('Mennyi buborekot szeretnel?')
+console.log (`${bubble1}`)
+console.log (`${bubble2}`)
 bubbles ()
